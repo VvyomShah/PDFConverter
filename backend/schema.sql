f396621726ef246206b842f1150f91d58865cc77
Vvyom Shah <vvyomms@gmail.com>
2025-05-04 14:35:25 -0400

    fix(DB): Removed default id setting from schema (sqlite does not have uuid)

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS jobs (
-    id TEXT PRIMARY KEY DEFAULT (lower(hex(randomblob(16)))), -- UUID-like
+    id TEXT PRIMARY KEY, -- UUID-like
     filename TEXT NOT NULL,
     status TEXT NOT NULL DEFAULT 'pending' CHECK (status IN ('pending', 'converting', 'completed', 'error')),
     error TEXT DEFAULT NULL,
